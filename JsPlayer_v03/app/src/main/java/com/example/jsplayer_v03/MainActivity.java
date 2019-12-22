2a5c6b39cd5ee96636faa43d5a4056edc9cc42ed
cowtwo <ct@test>
2019-12-22 01:27:23 +0000

    Hello World test ok

---
+++ JsPlayer_v03/app/src/main/java/com/example/jsplayer_v03/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.jsplayer_v03;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d("myTag", "This is my message");
+    }
+}
+
+
