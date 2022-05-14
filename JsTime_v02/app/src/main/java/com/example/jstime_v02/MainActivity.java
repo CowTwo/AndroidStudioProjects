b9623c53031d03e5123dc691b935d45015a98d64
root <root@crdhost-economic.us-central1-c.c.jsvm-debian.internal>
2022-05-14 23:50:04 +0000

    build JsTime_v02 (hello world) from android studio in gCloud VM

---
+++ JsTime_v02/app/src/main/java/com/example/jstime_v02/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.jstime_v02;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
